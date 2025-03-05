d1b3ceb0e97e78f867a52608f90d3be2454bd761
OiliaO <s.iliarabee2006@gmail.com>
2025-03-05 05:30:42 +0330

    complete Person class

--- src/base/Person.java
+++ src/base/Person.java
@@ -1,4 +1,25 @@
 package base;
+import java.util.*;
 
-public class person {
+public class Person {
+    public int id;
+    public String name;
+    public String nationalID;
+    public static ArrayList<Person> personList = new ArrayList<>();
+
+    public Person(String name, String nationalID) {
+        this.id = personList.size() + 1;
+        this.name = name;
+        this.nationalID = nationalID;
+        personList.add(this);
+    }
+
+    public static Person findByID(int id) {
+        for (Person person : personList) {
+            if (person.id == id) {
+                return person;
+            }
+        }
+        return null;
+    }
 }
