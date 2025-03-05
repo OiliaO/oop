21eda5d43fcd36352b6957eb1d3d16f23bd69ef0
OiliaO <s.iliarabee2006@gmail.com>
2025-03-05 05:39:41 +0330

    complete Course class

--- src/uni/Course.java
+++ src/uni/Course.java
@@ -1,4 +1,25 @@
 package uni;
+import java.util.ArrayList;
 
 public class Course {
+    public int id;
+    public String title;
+    public int units;
+    public static ArrayList<Course> courseList = new ArrayList<>();
+
+    public Course(String title, int units) {
+        this.id = courseList.size() + 1;
+        this.title = title;
+        this.units = units;
+        courseList.add(this);
+    }
+
+    public static Course findByID(int id) {
+        for (Course course : courseList) {
+            if (course.id == id) {
+                return course;
+            }
+        }
+        return null;
+    }
 }
