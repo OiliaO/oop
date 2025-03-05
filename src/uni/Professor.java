7813ff8a5eaa8217141cc821c1134455fe8c1e95
OiliaO <s.iliarabee2006@gmail.com>
2025-03-05 05:38:12 +0330

    complete Professor class

--- src/uni/Professor.java
+++ src/uni/Professor.java
@@ -1,4 +1,26 @@
 package uni;
+import base.Person;
+import java.util.*;
 
 public class Professor {
+    public int id;
+    public int personID;
+    public int majorID;
+    public static ArrayList<Professor> professorList = new ArrayList<>();
+
+    public Professor(int personID, int majorID) {
+        this.id = professorList.size() + 1;
+        this.personID = personID;
+        this.majorID = majorID;
+        professorList.add(this);
+    }
+
+    public static Professor findByID(int id) {
+        for (Professor professor : professorList) {
+            if (professor.id == id) {
+                return professor;
+            }
+        }
+        return null;
+    }
 }
