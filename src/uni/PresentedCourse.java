40de02343bbbb72f9e69e2c1785cf7ea6969ef72
OiliaO <s.iliarabee2006@gmail.com>
2025-03-05 05:41:41 +0330

    complete PresentedCourse class

--- src/uni/PresentedCourse.java
+++ src/uni/PresentedCourse.java
@@ -1,4 +1,37 @@
 package uni;
+import java.util.*;
 
 public class PresentedCourse {
+    public int id;
+    public int courseID;
+    public int professorID;
+    public int capacity;
+    public ArrayList<Integer> studentIds;
+    public static ArrayList<PresentedCourse> presentedCourseList = new ArrayList<>();
+
+    public PresentedCourse(int courseID, int professorID, int capacity) {
+        this.id = presentedCourseList.size() + 1;
+        this.courseID = courseID;
+        this.professorID = professorID;
+        this.capacity = capacity;
+        this.studentIds = new ArrayList<>(capacity);
+        presentedCourseList.add(this);
+    }
+
+    public static PresentedCourse findByID(int id) {
+        for (PresentedCourse presentedCourse : presentedCourseList) {
+            if (presentedCourse.id == id) {
+                return presentedCourse;
+            }
+        }
+        return null;
+    }
+
+    public void addStudent(int studentID) {
+        if (studentIds.size() < capacity) {
+            studentIds.add(studentID);
+        } else {
+            System.out.println("ظرفیت درس پر است");
+        }
+    }
 }
